26d76d1597cea325430c5150ad9b9de93c3cff39
Enzo Rodrigues <enzo.orodrigues03@gmail.com>
2025-05-11 14:02:55 -0300

    tech stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { AboutSection } from "@/components/layout/AboutSection";
 import { Container } from "@/components/layout/Container";
 import { HomeSection } from "@/components/layout/HomeSection";
+import { TechSection } from "@/components/layout/TechSection";
 import { TimelineSection } from "@/components/layout/TimelineSection";
 import { FloatingDock } from "@/components/ui/FloatingDock";
 import { items } from "@/components/ui/FloatingDock/items";
@@ -14,6 +15,7 @@ export default function Home() {
           <HomeSection />
           <AboutSection />
           <TimelineSection />
+          <TechSection />
 
 
           <FloatingDock desktopClassName="fixed bottom-2 left-1/2 -translate-x-1/2" mobileClassName="fixed bottom-3 right-3 " items={items}/>
---
+++ src/components/layout/TechSection/helper.tsx
@@ -0,0 +1,118 @@
+
+export const items = [
+
+  {
+    title: "Nodejs",
+    name: "nodejs",
+    icon: (
+      <>
+
+        <img height={60} width={60} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" />
+
+      </>
+    )
+  },
+
+
+  {
+    title: "React",
+    name: "reactjs",
+    icon: (
+      <>
+        <img height={60} width={60} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+      </>
+    )
+  },
+
+  {
+    title: "Next",
+    name: "nextjs",
+    icon: (
+      <>
+        <img height={60} width={60} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" />
+
+      </>
+    )
+  },
+
+  {
+    title: "Java",
+    name: "java",
+    icon: (
+      <>
+        <img height={60} width={60} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" />
+      </>
+    )
+  },
+
+  {
+    title: "Spring",
+    name: "springboot",
+    icon: (
+      <>
+
+        <img height={60} width={60} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg" />
+      </>
+    )
+  },
+
+  {
+    title: "Python",
+    name: "python",
+    icon: (
+      <>
+
+        <img height={60} width={60} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
+
+
+      </>
+    )
+  },
+
+  {
+    title: "Git",
+    name: "git",
+    icon: (
+      <>
+        <img height={60} width={60} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" />
+
+      </>
+    )
+  },
+
+  {
+    title: "Docker",
+    name: "docker",
+    icon: (
+      <>
+        <img height={60} width={60} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-plain.svg" />
+      </>
+    )
+  },
+
+          
+
+
+  {
+    title: "Arch Linux",
+    name: "archlinux",
+    icon: (
+      <>
+        <img height={60} width={60} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/archlinux/archlinux-original.svg" />
+
+      </>
+    )
+  },
+  
+
+
+  
+
+  
+
+  
+
+  
+
+  
+]
