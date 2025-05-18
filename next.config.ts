cd2425c2b0f5b402aeac643464c9800eee928842
Enzo Rodrigues <rodrigues@Enzos-MacBook-Pro.local>
2025-05-18 00:01:09 -0300

    Certifications

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  images : {
+    domains: [
+      "api.microlink.io"
+    ]
+  }
 };
 
 export default nextConfig;
